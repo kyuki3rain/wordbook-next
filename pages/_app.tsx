16d741cc10c9ea7acebb74cd20411a2af57e917c
kyuki3rain <kyuki3rain@gmail.com>
2021-08-28 17:09:56 +0900

    pwa対応?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="theme-color" content="#f9a8d4" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
